970b4b38ba2fa2aeb4a26ffcc717c541169fad5c
Admin <admin@Admins-Mac.local>
2018-03-25 01:45:30 +0530

    Created Test for GET and POST

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let morgan = require('morgan');
-let mongoose = require('mongoose');
+let mongoose = require('./models/mongoose');
 let bodyParser = require('body-parser');
 let path = require('path');
 let hbs  = require('hbs');
@@ -13,12 +13,6 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-mongoose.connect(credentials.database, (err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log('database is connected');
-});
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
--- credentials/credentials.js
+++ credentials/credentials.js
@@ -1,4 +1,5 @@
 module.exports = {
     database : 'mongodb://root:whitepanda@ds123499.mlab.com:23499/whitepanda',
+    testdatabase: 'mongodb://root:testcases@ds123399.mlab.com:23399/testcases',
     port : 3000
 }
