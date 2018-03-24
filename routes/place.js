9290f51076b1366f5b2ed52c513503af86769e9d
Admin <admin@Admins-Mac.local>
2018-03-24 14:58:26 +0530

    POST request done

--- routes/place.js
+++ routes/place.js
@@ -8,4 +8,20 @@ router.get('/places', (req,res)=>{
     })
 });
 
+router.post('/places', (req,res)=>{
+
+    let place = new Place();
+
+    place.Name=req.body.name;
+    place.Details.Price = req.body.price;
+    place.Details.Picture = req.body.picture;
+    place.Quantity = req.body.quantity;
+    
+    place.save((err)=>{
+        if(err) return err;
+
+        console.log(`added to database`);
+    })
+});
+
 module.exports = router;
