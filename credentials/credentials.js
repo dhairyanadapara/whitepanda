056ceb61f4cce4dfbe96b47e772150e2b26ae1ad
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2018-03-25 14:51:22 +0530

    Updated test database url

--- credentials/credentials.js
+++ credentials/credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
     database : 'mongodb://root:whitepanda@ds123499.mlab.com:23499/whitepanda',
-    testdatabase: 'mongodb://root:testcases@ds123399.mlab.com:23399/testcases',
+    testdatabase: 'mongodb://127.0.0.1:27017/PlacesTest',
     port : 3000
 }
